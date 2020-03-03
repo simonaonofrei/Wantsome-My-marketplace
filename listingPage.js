79ed371a46fee3d1bed41e3417d072d26af383b1
Simona <simona.bujor@ymail.com>
2020-03-03 21:52:22 +0200

    cards redesign done

--- listingPage.js
+++ listingPage.js
@@ -22,42 +22,69 @@ fetch(`http://localhost:3000/api/dresses${window.location.search}`, {
 
                 if (x.includes(searchValue.toUpperCase()) || y == searchValue) {
 
-                //identific div-ul la care tot adaug carduri -div-ul de baza
-                var basicDiv = document.getElementsByClassName("card-body")[0];
-                //creez noi tag-uri pentru elem din obiecte
-                var card = document.createElement("div");
-                card.classList.add("card"); card.classList.add("col-3"); card.classList.add("text-center");
-                card.classList.add(response[i].colour); card.classList.add(response[i].size);
-                var nameEl = document.createElement("h3");
-                var priceEl = document.createElement("p");
-                var imageEl = document.createElement("img");
-                var butonEl = document.createElement("a");
+                    //identific div-ul la care tot adaug carduri -div-ul de baza
+                    var basicDiv = document.getElementsByClassName("card-body")[0];
+                    //creez noi tag-uri pentru elem din obiecte
+                    var card = document.createElement("div");
+                    card.classList.add("col-md-4"); card.classList.add("product-grid");
+                    card.classList.add(response[i].colour); card.classList.add(response[i].size);
 
-                butonEl.setAttribute("href", "productDetail.html?section=");
-                butonEl.classList.add("btn"); butonEl.classList.add("btn-primary");
+                    var image = document.createElement("div");
+                    image.classList.add("image");
 
-                //stochez fiecare obiect in variabile
+                    var linkEl = document.createElement("a");
+                    linkEl.setAttribute("href", "productDetail.html?section=");
 
-                nameEl.innerText = response[i].name;
-                priceEl.innerText = response[i].price;
-                imageEl.innerText = response[i].image;
-                imageEl.setAttribute("src", response[i].image);
-                butonEl.innerText = "Details";
 
-                //adaug tagurile la card
-                card.appendChild(imageEl);
-                card.appendChild(nameEl);
-                card.appendChild(priceEl);
-                card.appendChild(butonEl);
-                //adaug cardul in pagina
-                basicDiv.append(card);
+                    var imageEl = document.createElement("img");
+                    imageEl.setAttribute("src", response[i].image);
+                    // imageEl.classList.add("w-100");
+                    imageEl.classList.add("text-center");
+                    imageEl.innerText = response[i].image;
+
+                    var overlay = document.createElement("div");
+                    overlay.classList.add("overlay");
+
+
+                    var detail = document.createElement("div");
+                    detail.classList.add("detail");
+                    detail.innerHTML = "View Details";
+
+
+                    var nameEl = document.createElement("h5");
+                    nameEl.classList.add("text-center");
+                    nameEl.innerText = response[i].name;
+
+
+                    var priceEl = document.createElement("h5");
+                    priceEl.classList.add("text-center");
+                    priceEl.innerText = response[i].price;
+
+                    var butonEl = document.createElement("a");
+                    butonEl.setAttribute("href", "#");
+                    butonEl.classList.add("btn"); butonEl.classList.add("buy");
+                    butonEl.innerText = "BUY";
+
+
+                    //adaug tagurile la card
+                    card.appendChild(image);
+
+                    //adaug cardul in pagina
+                    basicDiv.append(card);
+
+                    image.appendChild(linkEl);
+                    linkEl.appendChild(imageEl);
+                    linkEl.appendChild(overlay);
+                    overlay.appendChild(detail);
+                    card.appendChild(nameEl);
+                    card.appendChild(priceEl);
+                    card.appendChild(butonEl);
+
                 }
-            } 
-            // else if () {
-                
-            //   }
             }
-        
+
+        }
+
 
         // Condition for normal listing page
 
@@ -69,31 +96,59 @@ fetch(`http://localhost:3000/api/dresses${window.location.search}`, {
 
                 //creez noi tag-uri pentru elem din obiecte
                 var card = document.createElement("div");
-                card.classList.add("card"); card.classList.add("col-3"); card.classList.add("text-center");
+                card.classList.add("col-md-4"); card.classList.add("product-grid");
                 card.classList.add(response[i].colour); card.classList.add(response[i].size);
-                var nameEl = document.createElement("h3");
-                var priceEl = document.createElement("p");
+
+                var image = document.createElement("div");
+                image.classList.add("image");
+
+                var linkEl = document.createElement("a");
+                linkEl.setAttribute("href", "productDetail.html?section=");
+
+
                 var imageEl = document.createElement("img");
-                var butonEl = document.createElement("a");
-
-                butonEl.setAttribute("href", "productDetail.html?section=");
-                butonEl.classList.add("btn"); butonEl.classList.add("btn-primary");
-
-                //stochez fiecare obiect in variabile
-
-                nameEl.innerText = response[i].name;
-                priceEl.innerText = response[i].price;
-                imageEl.innerText = response[i].image;
                 imageEl.setAttribute("src", response[i].image);
-                butonEl.innerText = "Details";
+                // imageEl.classList.add("w-100");
+                imageEl.classList.add("text-center");
+                imageEl.innerText = response[i].image;
+
+                var overlay = document.createElement("div");
+                overlay.classList.add("overlay");
+
+
+                var detail = document.createElement("div");
+                detail.classList.add("detail");
+                detail.innerHTML = "View Details";
+
+
+                var nameEl = document.createElement("h5");
+                nameEl.classList.add("text-center");
+                nameEl.innerText = response[i].name;
+
+
+                var priceEl = document.createElement("h5");
+                priceEl.classList.add("text-center");
+                priceEl.innerText = response[i].price;
+
+                var butonEl = document.createElement("a");
+                butonEl.setAttribute("href", "#");
+                butonEl.classList.add("btn"); butonEl.classList.add("buy");
+                butonEl.innerText = "BUY";
+
 
                 //adaug tagurile la card
-                card.appendChild(imageEl);
+                card.appendChild(image);
+
+                //adaug cardul in pagina
+                basicDiv.append(card);
+
+                image.appendChild(linkEl);
+                linkEl.appendChild(imageEl);
+                linkEl.appendChild(overlay);
+                overlay.appendChild(detail);
                 card.appendChild(nameEl);
                 card.appendChild(priceEl);
                 card.appendChild(butonEl);
-                //adaug cardul in pagina
-                basicDiv.append(card);
 
 
             }
@@ -116,6 +171,15 @@ function accordion(section, heading, list) {
 };
 
 accordion('.filter-item', '.filter-item-inner-heading', '.filter-attribute-list');
+
+
+// Navigation bar
+$('ul.nav li.dropdown').hover(function () {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+}, function () {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+});
+
 
 // filter selector
 // var colorList = document.getElementById("colourFilter");   
