b3811b016495d69f04f7dc55487b3b54c0c773dc
Simona <simona.bujor@ymail.com>
2020-03-05 21:39:18 +0200

    Working page links - View details

--- productDetail.js
+++ productDetail.js
@@ -1,113 +1,71 @@
 // Autentificare cu token dupa logare - sign in
 fetch(`http://localhost:3000/api/dresses${window.location.search}`, {
-    headers: {
-        Authorization: `Bearer ${sessionStorage.getItem("token")}`
-    }
+  headers: {
+    Authorization: `Bearer ${sessionStorage.getItem("token")}`
+  }
 })
 
-.then(r => r.json())
-.then(function listItems(response) {
-if (window.location.href.includes("productId=")) {
-            let url = new URL(window.location);
-            let searchParams = new URLSearchParams(url.search);
-            var productId = searchParams.get("productId");
-            var response = response;
+  .then(r => r.json())
+  .then(function listItems(response) {
+    if (window.location.href.includes("productId=")) {
+      let url = new URL(window.location);
+      let searchParams = new URLSearchParams(url.search);
+      var productId = searchParams.get("productId");
+      var response = response;
 
-            for (let i = 0; i < response.length; i++) {
-              var x = response[i].name;
-              var y = response[i].id;
+      for (let i = 0; i < response.length; i++) {
+        var y = response[i].id;
+        if (y == productId) {
 
-              if (y === productId) {
+          //identific div-ul la care trebuie sa fac apend final
+          var mainDiv=document.getElementsByClassName("imagSlider")[0];
+          
 
-                  //identific div-ul care contine imaginea principala
-                  var divImag = document.getElementById("imagP");
-                  console.log(divImag);
-                  
-                  //creez noi tag-uri pentru elem din obiecte
-                  var card = document.createElement("div");
-                  card.classList.add("col-md-4"); card.classList.add("product-grid");
-                  card.classList.add(response[i].colour); card.classList.add(response[i].size);
-
-                  var image = document.createElement("div");
-                  image.classList.add("image");
-
-                  var linkEl = document.createElement("a");
-                  linkEl.setAttribute("href", "productDetail.html?id=" + response[i].id);
+          //identific div-ul care contine imaginea principala
+          var divImag = document.querySelector('div>img');
+          divImag.setAttribute("src", response[i].image);
 
 
-                  var imageEl = document.createElement("img");
-                  imageEl.setAttribute("src", response[i].image);
-                  // imageEl.classList.add("w-100");
-                  imageEl.classList.add("text-center");
-                  imageEl.innerText = response[i].image;
+          //identific div-ul cu product description
+          var productNameDiv = document.getElementsByClassName("product-description")[0];
+          
+          //identific elementul-ul care contine denumirea produsului
+          var productName = productNameDiv.getElementsByTagName("h1")[0];
+          productName.innerHTML=response[i].name;
 
-                  var overlay = document.createElement("div");
-                  overlay.classList.add("overlay");
+          var productID = document.querySelectorAll('div > p')[0];
+          productID.innerHTML= "Cod produs: " + response[i].id;
 
-
-                  var detail = document.createElement("div");
-                  detail.classList.add("detail");
-                  detail.innerHTML = "View Details";
-
-
-                  var nameEl = document.createElement("h5");
-                  nameEl.classList.add("text-center");
-                  nameEl.innerText = response[i].name;
-
-
-                  var priceEl = document.createElement("h5");
-                  priceEl.classList.add("text-center");
-                  priceEl.innerText = "$ " + response[i].price;
-
-                  var butonEl = document.createElement("a");
-                  butonEl.setAttribute("href", "#");
-                  butonEl.classList.add("btn"); butonEl.classList.add("buy"); butonEl.setAttribute("data-name", response[i].name);
-                  butonEl.setAttribute("data-price", response[i].price); butonEl.setAttribute("data-id", response[i].id);
-                
-                  butonEl.innerText = "BUY";
-
-
-                  //adaug tagurile la card
-                  card.appendChild(image);
-
-                  //adaug cardul in pagina
-                  basicDiv.append(card);
-
-                  image.appendChild(linkEl);
-                  linkEl.appendChild(imageEl);
-                  linkEl.appendChild(overlay);
-                  overlay.appendChild(detail);
-                  card.appendChild(nameEl);
-                  card.appendChild(priceEl);
-                  card.appendChild(butonEl);
-
-              }
-          }
-
+          var productPriceDiv = document.getElementsByClassName("product-price")[0];
+          var productPrice = productPriceDiv.getElementsByTagName("span")[0];
+          productPrice.innerHTML= "$" + response[i].price;       
+        }
       }
-    });
+
+    }
+  });
 
 
 
 // Navigation bar
 $('ul.nav li.dropdown').hover(function () {
-    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
-  }, function () {
-    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
-  });
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+}, function () {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+});
 
 
 //Details part
-$(document).ready(function(){
-	
-	$('ul.tabs li').click(function(){
-		var tab_id = $(this).attr('data-tab');
+$(document).ready(function () {
 
-		$('ul.tabs li').removeClass('current');
-		$('.tab-content').removeClass('current');
+  $('ul.tabs li').click(function () {
+    var tab_id = $(this).attr('data-tab');
 
-		$(this).addClass('current');
-		$("#"+tab_id).addClass('current');
-	})
+    $('ul.tabs li').removeClass('current');
+    $('.tab-content').removeClass('current');
+
+    $(this).addClass('current');
+    $("#" + tab_id).addClass('current');
+  })
 
 })
